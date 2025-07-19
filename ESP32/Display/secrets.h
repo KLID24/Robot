f16b17017ad6a1e30310c71f1b48c01ec946efd5
KLID <kakouto_pasan@outlook.com>
2025-07-19 15:23:39 -0300

    Second com

--- ESP32/Display/secrets.h
+++
@@ -1,5 +0,0 @@
-#define WIFI_SSID "Personal-661"
-#define WIFI_PASSWORD "0018781896"
-#define API_KEY "AIzaSyDm52J6WTHvZyqRU4czpy5lYyuZ0TVykLQ"
-#define DATABASE_URL "https://srpapa-database-default-rtdb.firebaseio.com/"
-#define USER_PATH "/user"
