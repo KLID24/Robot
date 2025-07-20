b92fbfdaf7ac1a4062a503a58aa3307c20915d46
cebem1nt <mineewarik@gmail.com>
2025-07-20 11:43:28 -0300

    Refactoring, esp code now in Display/Display.ino

---
+++ Display/secrets.h
@@ -0,0 +1,9 @@
+#ifndef ESP32_HEADER_SECRETS
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define API_KEY ""
+#define DATABASE_URL ""
+
+#endif
--- esp/secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESP32_HEADER_SECRETS
-
-#define WIFI_SSID "Personal-661"
-#define WIFI_PASSWORD "0018781896"
-
-#define API_KEY "AIzaSyDm52J6WTHvZyqRU4czpy5lYyuZ0TVykLQ"
-#define DATABASE_URL "https://srpapa-database-default-rtdb.firebaseio.com/"
-
-#endif
